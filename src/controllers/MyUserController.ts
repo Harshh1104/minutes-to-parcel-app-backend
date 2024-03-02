a143684d5c22bd9b322f6c86652d9e2b2650c076
Harshh1104 <verma.harsh1512@gmail.com>
2024-03-02 10:53:04 +0530

    first commit

---
+++ src/controllers/MyUserController.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+import User from "../models/user";
+
+const getCurrentUser = async (req: Request, res: Response) => {
+    try{
+
+        const currentUser = await User.findOne({ _id: req.userId});
+        if(!currentUser){
+            return res.status(404).json({message:"User not found"});
+        }
+
+        res.json(currentUser);
+
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({ message: "Something went wrong!"});
+    }
+}
+
+const createCurrentUser = async (req: Request, res: Response) => {
+   
+    try {
+        const { auth0Id } = req.body;
+        const existingUser = await User.findOne({ auth0Id });
+
+         //1. check if the user exists
+        if(existingUser){
+            return res.status(200).send();
+        }
+
+        //2. create the user if it doesn't exist
+        const newUser = new User(req.body);
+        await newUser.save();
+
+        //3. return the user object to the calling client
+        res.status(201).json(newUser.toObject());
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Error creating the user"
+        });
+    }
+};
+
+const updateCurrentUser = async (req: Request, res: Response) => {
+    try{
+        const { name, addressLine1, country, city } = req.body;
+        const user = await User.findById(req.userId);
+
+        if(!user){
+            return res.status(404).json({message: "User not found"});
+        }
+
+        user.name = name;
+        user.addressLine1 = addressLine1;
+        user.city = city;
+        user.country = country;
+        await user.save();
+
+        res.send(user); 
+    } catch(error){
+        console.log(error);
+        res.status(500).json({message: "Error updating the user"});
+    }
+};
+
+export default {
+    createCurrentUser,
+    updateCurrentUser,
+    getCurrentUser,
+};
