891f8b7650e1eccbb770c7ffc0f3257e2fb5e255
Harshh1104 <verma.harsh1512@gmail.com>
2024-03-03 22:58:53 +0530

    adding user profile feature

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -23,16 +23,13 @@ const createCurrentUser = async (req: Request, res: Response) => {
         const { auth0Id } = req.body;
         const existingUser = await User.findOne({ auth0Id });
 
-         //1. check if the user exists
         if(existingUser){
             return res.status(200).send();
         }
 
-        //2. create the user if it doesn't exist
         const newUser = new User(req.body);
         await newUser.save();
 
-        //3. return the user object to the calling client
         res.status(201).json(newUser.toObject());
     } catch (error) {
         console.log(error);
